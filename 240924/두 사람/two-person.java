6b235314f6e65fe42302501c2812e623694a645d
Hwangseoeun <129192366+Hwangseoeun@users.noreply.github.com>
2024-09-24 08:02:13 +0900

    Title: 두 사람, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240924/두 사람/two-person.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aAge = sc.nextInt();
+        String aGender = sc.next();
+        int bAge = sc.nextInt();
+        String bGender = sc.next();
+
+        if(aAge>=19 && aGender.equals("M")){
+            System.out.print(1);
+        }
+        else if(bAge>=19 && bGender.equals("M")){
+            System.out.print(1);
+        }
+        else{
+            System.out.print(0);
+        }
+    }
+}
