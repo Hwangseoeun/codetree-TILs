f2071faac42ed0a5f9ec9f5cb83284917a54f5ba
Hwangseoeun <129192366+Hwangseoeun@users.noreply.github.com>
2024-09-24 20:14:28 +0900

    Title: 별 n개 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240924/별 n개 출력하기/print-n-stars.java
+++ 240924/별 n개 출력하기/print-n-stars.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int i=1;
 
-        for(int i=0; i<n; i++){
+        while(i<=n){
             System.out.println("*");
+            i++;
         }
     }
 }
