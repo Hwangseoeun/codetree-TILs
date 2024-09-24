fc381ee7311d0e22b7f132865e9c1bcf3d8b24e4
Hwangseoeun <129192366+Hwangseoeun@users.noreply.github.com>
2024-09-24 20:43:01 +0900

    Title: 반복 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240924/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        printText(N);
+    }
+
+    public static void printText(int num){
+        for(int i=0; i<num; i++){
+            System.out.println("12345^&*()_");
+        }
+    }
+}
