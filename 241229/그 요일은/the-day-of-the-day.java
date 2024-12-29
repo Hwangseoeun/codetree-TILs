b6f72ff5aa0a1c8006e69478acecdc9cb0a946a9
Hwangseoeun <sunny030921@gmail.com>
2024-12-29 11:01:14 +0900

    Title: 그 요일은, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241229/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        String A = sc.next();
+
+        int[] day = new int[]{0,31,29,31,30,31,30,31,31,30,31,30,31};
+
+        int first = 0;
+        for(int i=1; i<m1; i++) {
+            first += day[i];
+        }
+        first += d1;
+
+        int second = 0;
+        for(int i=1; i<m2; i++) {
+            second += day[i];
+        }
+        second += d2;
+
+        int num = 0;
+        switch(A) {
+            case "Mon":
+                num = 6;
+                break;
+            case "Tue":
+                num = 2;
+                break;
+            case "Wed":
+                num = 3;
+                break;
+            case "Thu":
+                num = 4;
+                break;
+            case "Fri":
+                num = 5;
+                break;
+            case "Sat":
+                num = 6;
+                break;
+            case "Sun":
+                num = 0;
+                break;
+        }
+
+        if((second-first)%7 <= num) {
+            System.out.println((second-first)/6);
+        }
+        else {
+            System.out.println((second-first)/8);
+        }
+    }
+}
