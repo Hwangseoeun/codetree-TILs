9d51575bf354937711bc5a9f7e1ede2bbbf1be74
Hwangseoeun <sunny030921@gmail.com>
2025-01-08 23:14:58 +0900

    Title: 신기한 타일 뒤집기, Time: 211ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250108/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String[] tile = new String[200000];
+        int cur = 100000, whiteTileCount = 0, blackTileCount = 0;
+
+        for(int i=0; i<n; i++) {
+            int num = sc.nextInt();
+            String dir = sc.next();
+
+            if(dir.equals("L")) {
+                for(int j=cur; j>cur-num; j--) {
+                    tile[j] = "WHITE";
+                }
+
+                cur -= num+1;
+            }
+
+            else {
+                for(int j=cur; j<cur+num; j++) {
+                    tile[j] = "BLACK";
+                }
+
+                cur += num-1;
+            }
+        }
+
+        for(int i=0; i<200000; i++) {
+            if(tile[i] != null && tile[i].equals("WHITE")) {
+                whiteTileCount++;
+            }
+
+            if(tile[i] != null && tile[i].equals("BLACK")) {
+                blackTileCount++;
+            }
+        }
+
+        System.out.print(whiteTileCount + " " + blackTileCount);
+    }
+}
