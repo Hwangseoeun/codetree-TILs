b376f96c7cfe90c4f72bae99c637644ddcf03ef0
Hwangseoeun <sunny030921@gmail.com>
2025-03-11 13:29:19 +0900

    Title: 선두를 지켜라, Time: 132ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250311/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int aLen = 1, bLen = 1;
+        int[] A = new int[1000000];
+        int[] B = new int[1000000];
+
+        A[0] = 0;
+        B[0] = 0;
+
+        for(int i=0; i<N; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for(int j=aLen; j<aLen+t; j++) {
+                A[j] = A[j-1] + v;
+            }
+
+            aLen += t;
+        }
+
+        // for(int i=0; i<aLen; i++) {
+        //     System.out.print(A[i] + " ");
+        // }
+        // System.out.println();
+
+        for(int i=0; i<M; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for(int j=bLen; j<bLen+t; j++) {
+                B[j] = B[j-1] + v;
+            }
+
+            bLen += t;
+        }
+
+        // for(int i=0; i<bLen; i++) {
+        //     System.out.print(B[i] + " ");
+        // }
+        // System.out.println();
+
+        boolean[] head = new boolean[aLen];
+        for(int i=1; i<aLen; i++) {
+            if(A[i]>B[i]) {
+                head[i] = true;
+            }
+            else if(A[i]<B[i]) {
+                head[i] = false;
+            }
+            else if(A[i]==B[i] && i!=1){
+                head[i] = head[i-1];
+            }    
+        }
+
+        int cnt = 0;
+        for(int i=1; i<aLen; i++) {
+            if(i!=1 && head[i-1] != head[i]) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
