862559cb49b30e1f4c48158edb22d65fc1116ea7
Hwangseoeun <sunny030921@gmail.com>
2025-05-08 12:48:10 +0900

    Title: 좌우로 움직이는 로봇, Time: 138ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250508/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] A = new int[1000000];
+        int[] B = new int[1000000];
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int aIdx = 0, bIdx = 0, aLen = 0, bLen = 0;
+        A[0] = 0;
+        B[0] = 0;
+
+        for(int i=0; i<N; i++) {
+            int t = sc.nextInt();
+            String d = sc.next();
+
+            for(int j=aIdx+1; j<=aIdx+t; j++) {
+                if(d.equals("L")) {
+                    A[j] = --aLen;
+                }
+                else {
+                    A[j] = ++aLen;
+                }
+            }
+
+            aIdx += t;
+        }
+
+        for(int i=0; i<M; i++) {
+            int t = sc.nextInt();
+            String d = sc.next();
+
+            for(int j=bIdx+1; j<=bIdx+t; j++) {
+                if(d.equals("L")) {
+                    B[j] = --bLen;
+                }
+                else {
+                    B[j] = ++bLen;
+                }
+            }
+
+            bIdx += t;
+        }
+
+        int cnt = 0;
+
+        for(int i=1; i<Math.max(aIdx, bIdx)-1; i++) {
+            if(A[i-1]==A[i+1] && A[i]!=A[i+1]) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
